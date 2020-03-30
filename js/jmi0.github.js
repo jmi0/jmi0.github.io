9fd0a632956c01daa11d264ef73cdd179d221020
Joseph Iannone <joseph.m.iannone@gmail.com>
2020-03-30 08:32:33 -0400

    initial commit

--- js/jmi0.github.js
+++ js/jmi0.github.js
@@ -1,3 +1,12 @@
+/**
+ * @Author: Joe Iannone <josephiannone>
+ * @Date:   2020-03-29T21:24:06-04:00
+ * @Filename: jmi0.github.js
+ * @Last modified by:   josephiannone
+ * @Last modified time: 2020-03-30T08:30:37-04:00
+ */
+
+
 
 console.log('Hey Neighbor!');
 
@@ -6,17 +15,18 @@ $(document).ready(function() {
   $('#profile-container').hide();
   $('#profile-container').fadeIn(700);
 
+  /*
   // center align
   //$('#profile-container').css({top: getTop(), left: getLeft()});
-
   $(window).resize(function() {
     // keep this center aligned
     //$('#profile-container').css({top: getTop(), left: getLeft()});
   });
+  */
 
 });
 
-
+/*
 function getTop() {
   let top = ($(window).height() / 2) - ($('#profile-container').height() / 2);
   if (top < 0) top = 0;
@@ -28,3 +38,4 @@ function getLeft() {
   if (left < 0) left = 0;
   return left;
 }
+*/
